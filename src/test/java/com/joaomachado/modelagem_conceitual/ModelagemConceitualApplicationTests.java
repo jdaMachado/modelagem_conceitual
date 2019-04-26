ac4a65003901541ed5d9c7ad5812a30380379b7a
Joao Machado <joao_-machado@hotmail.com>
2019-04-26 15:09:20 +0000

    Projecto Criado

---
+++ src/test/java/com/joaomachado/modelagem_conceitual/ModelagemConceitualApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joaomachado.modelagem_conceitual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelagemConceitualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
